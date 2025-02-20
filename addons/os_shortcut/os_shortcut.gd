03c6c5ae6e3e14d8e9aab68e2623376cc8ad6aba
thiagola92 <thiagola92@gmail.com>
2025-02-20 13:02:23 -0300

    Stop showing key as modifier when key is echoing

--- addons/os_shortcut/os_shortcut.gd
+++ addons/os_shortcut/os_shortcut.gd
@@ -46,10 +46,10 @@ func _on_key_press(keycode: int) -> void:
 	var event_key = InputEventKey.new()
 	event_key.keycode = keycode
 	event_key.pressed = true
-	event_key.alt_pressed = KEY_ALT in keys_down
-	event_key.ctrl_pressed = KEY_CTRL in keys_down
-	event_key.meta_pressed = KEY_META in keys_down
-	event_key.shift_pressed = KEY_SHIFT in keys_down
+	event_key.alt_pressed = KEY_ALT in keys_down and keycode != KEY_ALT
+	event_key.ctrl_pressed = KEY_CTRL in keys_down and keycode != KEY_CTRL
+	event_key.meta_pressed = KEY_META in keys_down and keycode != KEY_META
+	event_key.shift_pressed = KEY_SHIFT in keys_down and keycode != KEY_SHIFT
 	
 	if keycode not in keys_down:
 		keys_down.append(keycode)
@@ -60,17 +60,14 @@ func _on_key_press(keycode: int) -> void:
 
 
 func _on_key_release(keycode: int) -> void:
-	# First remove the key, so it doesn't mention it own modifier.
-	# For example, pressing and releasing Ctrl shouldn't
-	# set "ctrl_pressed" unless another Ctrl is pressed.
-	keys_down.erase(keycode)
-	
 	var event_key = InputEventKey.new()
 	event_key.keycode = keycode
 	event_key.pressed = false
-	event_key.alt_pressed = KEY_ALT in keys_down
-	event_key.ctrl_pressed = KEY_CTRL in keys_down
-	event_key.meta_pressed = KEY_META in keys_down
-	event_key.shift_pressed = KEY_SHIFT in keys_down
+	event_key.alt_pressed = KEY_ALT in keys_down and keycode != KEY_ALT
+	event_key.ctrl_pressed = KEY_CTRL in keys_down and keycode != KEY_CTRL
+	event_key.meta_pressed = KEY_META in keys_down and keycode != KEY_META
+	event_key.shift_pressed = KEY_SHIFT in keys_down and keycode != KEY_SHIFT
+	
+	keys_down.erase(keycode)
 	
 	Input.parse_input_event(event_key)
