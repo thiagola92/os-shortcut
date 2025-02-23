94d7baf0da1348838e56b606a2be7eec75ce3fa0
thiagola92 <thiagola92@gmail.com>
2025-02-23 12:37:45 -0300

    Flush inputs

--- addons/os_shortcut/os_shortcut.gd
+++ addons/os_shortcut/os_shortcut.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-var keys_down: Array = []
+var _keys_down: Array = []
 
 
 func _ready() -> void:
@@ -14,10 +14,19 @@ func _exit_tree() -> void:
 
 func _process(_delta: float) -> void:
 	# Get event from queue.
-	var event: OSEvent = OSListener.get_singleton().get_event()
+	var event: InputEvent = OSListener.get_singleton().get_event()
 	
 	if not event:
 		return
+	
+	if event is InputEventKey:
+		_process_event_key(event)
+
+
+func _process_event_key(event: InputEventKey) -> void:
+	# On Windows, it is always the Alt key.
+	if OS.get_name() == "Windows" and event.keycode == KEY_MENU:
+		event.keycode = KEY_ALT
 	
 	# INFO:
 	# On Windows, Godot CONSUME events while window has focus, which means
@@ -27,47 +36,26 @@ func _process(_delta: float) -> void:
 	#
 	# So to avoid receiving two times the same input on Linux,
 	# we ignore when the window has focus.
-	#
-	# NOTE: If the user press a key while focusing another window and
-	# release in godot window, we still needs to remove the key.
 	if get_window().has_focus():
-		if event.type == event.KEY_RELEASE:
-			keys_down.erase(event.code)
+		# ATTENTION: If the user press a key while focusing another window and
+		# release in godot window, we still needs to remove the key.
+		if not event.pressed:
+			_keys_down.erase(event.keycode)
+		
 		return
 	
-	match event.type:
-		event.KEY_PRESS:
-			_on_key_press(event.code)
-		event.KEY_RELEASE:
-			_on_key_release(event.code)
-
-
-func _on_key_press(keycode: int) -> void:
-	var event_key = InputEventKey.new()
-	event_key.keycode = keycode
-	event_key.pressed = true
-	event_key.alt_pressed = KEY_ALT in keys_down and keycode != KEY_ALT
-	event_key.ctrl_pressed = KEY_CTRL in keys_down and keycode != KEY_CTRL
-	event_key.meta_pressed = KEY_META in keys_down and keycode != KEY_META
-	event_key.shift_pressed = KEY_SHIFT in keys_down and keycode != KEY_SHIFT
+	event.alt_pressed = KEY_ALT in _keys_down and event.keycode != KEY_ALT
+	event.ctrl_pressed = KEY_CTRL in _keys_down and event.keycode != KEY_CTRL
+	event.meta_pressed = KEY_META in _keys_down and event.keycode != KEY_META
+	event.shift_pressed = KEY_SHIFT in _keys_down and event.keycode != KEY_SHIFT
 	
-	if keycode not in keys_down:
-		keys_down.append(keycode)
+	if event.pressed:
+		if event.keycode not in _keys_down:
+			_keys_down.append(event.keycode)
+		else:
+			event.echo = true
 	else:
-		event_key.echo = true
+		_keys_down.erase(event.keycode)
 	
-	Input.parse_input_event(event_key)
-
-
-func _on_key_release(keycode: int) -> void:
-	var event_key = InputEventKey.new()
-	event_key.keycode = keycode
-	event_key.pressed = false
-	event_key.alt_pressed = KEY_ALT in keys_down and keycode != KEY_ALT
-	event_key.ctrl_pressed = KEY_CTRL in keys_down and keycode != KEY_CTRL
-	event_key.meta_pressed = KEY_META in keys_down and keycode != KEY_META
-	event_key.shift_pressed = KEY_SHIFT in keys_down and keycode != KEY_SHIFT
-	
-	keys_down.erase(keycode)
-	
-	Input.parse_input_event(event_key)
+	Input.parse_input_event(event)
+	Input.flush_buffered_events()
--- test/main.gd
+++ test/main.gd
@@ -4,10 +4,5 @@ extends Control
 # TESTING
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if not event.echo:
+		#if not event.echo:
 			print(event)
-
-
-# TESTING
-func _on_button_pressed() -> void:
-	print("BUTTON PRESSED")
