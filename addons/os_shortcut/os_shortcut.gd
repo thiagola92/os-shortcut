6ef05c71fe7f98078623599636251ca07b1282ca
thiagola92 <thiagola92@gmail.com>
2025-02-23 22:54:47 -0300

    Remove logic to know keys pressed

--- addons/os_shortcut/os_shortcut.gd
+++ addons/os_shortcut/os_shortcut.gd
@@ -1,9 +1,6 @@
 extends Node
 
 
-var _keys_down: Array = []
-
-
 func _ready() -> void:
 	OSListener.get_singleton().start_listen()
 
@@ -37,25 +34,12 @@ func _process_event_key(event: InputEventKey) -> void:
 	# So to avoid receiving two times the same input on Linux,
 	# we ignore when the window has focus.
 	if get_window().has_focus():
-		# ATTENTION: If the user press a key while focusing another window and
-		# release in godot window, we still needs to remove the key.
-		if not event.pressed:
-			_keys_down.erase(event.keycode)
-		
 		return
 	
-	event.alt_pressed = KEY_ALT in _keys_down and event.keycode != KEY_ALT
-	event.ctrl_pressed = KEY_CTRL in _keys_down and event.keycode != KEY_CTRL
-	event.meta_pressed = KEY_META in _keys_down and event.keycode != KEY_META
-	event.shift_pressed = KEY_SHIFT in _keys_down and event.keycode != KEY_SHIFT
-	
-	if event.pressed:
-		if event.keycode not in _keys_down:
-			_keys_down.append(event.keycode)
-		else:
-			event.echo = true
-	else:
-		_keys_down.erase(event.keycode)
+	event.alt_pressed = Input.is_key_pressed(KEY_ALT) and event.keycode != KEY_ALT
+	event.ctrl_pressed = Input.is_key_pressed(KEY_CTRL) and event.keycode != KEY_CTRL
+	event.meta_pressed = Input.is_key_pressed(KEY_META) and event.keycode != KEY_META
+	event.shift_pressed = Input.is_key_pressed(KEY_SHIFT) and event.keycode != KEY_SHIFT
 	
 	Input.parse_input_event(event)
 	Input.flush_buffered_events()
